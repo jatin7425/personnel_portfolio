d135557b6aefe628bbf2e802b015c8517b1fabbd
Jatin Vishwakarma <jatinvishwakarma4310@gmail.com>
2025-08-16 13:52:40 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/personnel_portfolio', // adjust to your repo name
+};
+
+module.exports = nextConfig;
