abc759318f622e4e63c03d8bb2c76246c2284325
Jatin Vishwakarma <jatinvishwakarma4310@gmail.com>
2025-08-16 09:26:04 +0000

    Add experimental options and clarify image optimization in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true, // required for GH Pages, since it can't run the image optimizer
+  },
+  experimental: {
+    // for future versions, add experimental options here if needed
+  },
 };
 
 export default nextConfig;
