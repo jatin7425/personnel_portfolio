bcd2de738c394f71dfa4d31e96816199635631c6
Jatin Vishwakarma <jatinvishwakarma4310@gmail.com>
2025-08-16 09:16:32 +0000

    Remove unnecessary configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, // required for GH Pages, since it can't run the image optimizer
-    },
-    basePath: '/personnel_portfolio',
 };
 
 export default nextConfig;
